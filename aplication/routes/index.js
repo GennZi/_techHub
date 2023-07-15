ec6f2f65a4992c24bf79e7686496ede8070e651c
Joubert Gruna <67075331+joubertgruna@users.noreply.github.com>
2023-07-14 23:07:26 -0300

    up

--- aplication/routes/index.js
+++ aplication/routes/index.js
@@ -3,27 +3,27 @@ var router = express.Router();
 
 /* GET Builder aplication main Landing page. */
 router.get('/builder', function(req, res, next) {
-  res.render('./builder/index', { title: 'Tech Hub' });
+  res.render('./builder/index', { title: 'TecHub' });
 });
 
 /* GET about page. */
 router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'Tech Hub' });
+  res.render('about', { title: 'TecHub' });
 });
 
 /* GET portfolio page. */
 router.get('/portfolio', function(req, res, next) {
-  res.render('portfolio', { title: 'Tech Hub', header: 'Serviços que oferecemos'});
+  res.render('portfolio', { title: 'TecHub', header: 'Serviços que oferecemos'});
 });
 
 /* GET services page. */
 router.get('/services', function(req, res, next) {
-  res.render('services', { title: 'Tech Hub', header: 'Serviços que oferecemos'});
+  res.render('services', { title: 'TecHub', header: 'Serviços que oferecemos'});
 });
 
 /* GET services page. */
 router.get('/contact', function(req, res, next) {
-  res.render('contact', { title: 'Tech Hub', header: 'Serviços que oferecemos'});
+  res.render('contact', { title: 'TecHub', header: 'Serviços que oferecemos'});
 });
 
 module.exports = router;
